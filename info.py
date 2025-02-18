6c0d740f476cf3fc2b3e08f031e16d9b29364adc
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2025-02-19 02:24:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,7 +58,7 @@ LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
 auth_channel = environ.get('AUTH_CHANNEL', '')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 'https://t.me/mv_backup'))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002091966447'))
 
 # bot settings
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
